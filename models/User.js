269ebdeb18281593c01e3274cfdc62d46f653c5a
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-08-14 21:28:06 -0400

    create/add users and awards!

--- models/User.js
+++ models/User.js
@@ -17,21 +17,16 @@ const User = db.define('user', {
 User.findUsersViewModel = () => {
   return User.findAll({ // w/ eager loading to get associations
     // if the assocation is aliased (using the as option), you must specify this alias
-    // include: [
-    //   { model: User, as: 'mentor' },
-    //   { model: db.models.Award }, // pulling in from index.js -> models: { User, Award } export?
-    // ]
+    include: [
+      { model: User, as: 'mentor' },
+      { model: db.models.award }, // pulling in from index.js -> models: { User, Award } export?
+    ]
   })
     .then( (users) => {
-      console.log('findUsersViewModel results = ', users);
+      // console.log('findUsersViewModel results = ', users);
       return users;
     });
 };
-
-// User.create(req.body)
-// User.create = (user) => {
-
-// };
 
 // User.destroyById(req.params.id)
 User.destroyById = (id) => {
@@ -45,18 +40,13 @@ User.updateUserFromRequestBody = (id, body) => {
 
 // User.generateAward(req.params.id)
 User.generateAward = (id) => {
-  return db.models.Award.create({ description: faker.company.catchPhrase(), userId: id });
+  return db.models.award.create({ description: faker.company.catchPhrase(), userId: id });
 };
 
 // User.removeAward(req.params.userId, req.params.id)
 User.removeAward = (userId, awardId) => {
-  return db.models.Award.destroy({ where: { id: awardId }});
+  return db.models.award.destroy({ where: { id: awardId }});
 };
-
-// // Create!
-// User.prototype.create = (name) => {
-//   return db.models.User.create({ name: name });
-// };
 
 // exports
 module.exports = User;
